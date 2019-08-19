2d7832c641a5dbe13baec3539df9ad1fce7aab17
martypowell <jpowel25@gmail.com>
2019-08-19 15:00:30 -0400

    fixed logical issue to allow us to access the name

--- src/Handlebars/index.js
+++ src/Handlebars/index.js
@@ -2,8 +2,9 @@ import Handlebars from 'handlebars';
 import { limit, skip } from './HandlebarsHelpers';
 
 /** Register all the helpers */
-[ limit, skip ].forEach((helper) => {
-	Handlebars.registerHelper(helper.name, helper);
+const exportedHelpers = { limit, skip };
+Object.keys(exportedHelpers).forEach((helperKey) => {
+	Handlebars.registerHelper(helperKey, exportedHelpers[helperKey]);
 });
 
 export default Handlebars;
