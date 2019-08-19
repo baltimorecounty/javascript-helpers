504318c227165530d213e84c1a7e855db47cdf6a
martypowell <jpowel25@gmail.com>
2019-08-19 08:54:39 -0400

    fixed test to match export change

--- src/CollapseHelper.test.js
+++ src/CollapseHelper.test.js
@@ -1,5 +1,6 @@
 import CollapseHelper from './CollapseHelper';
 
 test('exports a11y-toggle module from npm', () => {
-	expect(CollapseHelper).toBeDefined();
+	expect(window.a11yToggle).toBeDefined();
+	expect(typeof window.a11yToggle).toEqual('function');
 });
